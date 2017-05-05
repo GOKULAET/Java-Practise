d7e14f73f28abb41ae5998b5cac0ac04f66fb1a5
Gokul Raj <gokulaet@gmail.com>
2017-05-06 00:54:35 +0530

    Create StarTriangle.java

---
+++ StarTriangle.java
@@ -0,0 +1,25 @@
+public class StarTriangle{
+           //static int a= 1;
+     public static void main(String []args){
+        int n=5;
+        for(int i=0;i<n;i++)
+        {
+            int k=i;
+        for(int j=0;j<=k;j++)
+        {
+            System.out.print("* "); 
+         //System.out.print(" "+a); 
+         //a=a+1;
+        }
+        System.out.println(""); 
+        }
+     }
+}
+
+
+//OUTPUT
+*                                                                                                                                                                
+* *                                                                                                                                                              
+* * *                                                                                                                                                            
+* * * *                                                                                                                                                          
+* * * * *   
