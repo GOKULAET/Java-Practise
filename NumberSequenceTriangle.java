5963d64424c65e771c97e06ac7b75e292b65c9fe
Gokul Raj <gokulaet@gmail.com>
2017-05-06 00:57:50 +0530

    Create NumberSequenceTriangle.java

---
+++ NumberSequenceTriangle.java
@@ -0,0 +1,26 @@
+public class HelloWorld{
+    
+        static int a= 1;
+     public static void main(String []args){
+        
+        int n=5;
+        for(int i=0;i<n;i++)
+        {
+            int k=i;
+        for(int j=0;j<=k;j++)
+        {  
+         System.out.print(" "+a); 
+         a=a+1;
+        }
+        System.out.println(""); 
+        }
+     }
+}
+
+//OUTPUT
+
+ 1                                                                                                                                                               
+ 2 3                                                                                                                                                             
+ 4 5 6                                                                                                                                                           
+ 7 8 9 10                                                                                                                                                        
+ 11 12 13 14 15
