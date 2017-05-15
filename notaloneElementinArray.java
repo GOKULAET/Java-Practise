1f4eff4f4449eb7323ca9b2ce11431eb91382962
Gokul Raj <gokulaet@gmail.com>
2017-05-15 09:13:19 +0530

    Create notaloneElementinArray.java
    We'll say that an element in an array is "notalone" if there are values before and after it, and those values are different from it. Return a version of the given array where every instance of the given value which is notalone is replaced by whichever value to its left or right is larger.
    Sample Input: 1. notAlone({1, 2, 3}, 2) -> {1, 3, 3} , 2. notAlone({1, 2, 3, 2, 5, 2}, 2)->{1, 3, 3, 5, 5, 2} ,
    Sample Output: 3. notAlone({3, 4}, 3) -> {3, 4}

---
+++ notaloneElementinArray.java
@@ -0,0 +1,28 @@
+public int[] notAlone(int[] nums, int val){
+int[] result=nums;
+for(int i=1;i<=(nums.length-2);i++){
+if((nums[i]==val) && (nums[i-1]<nums[i+1]))
+result[i]=nums[i+1];
+if((nums[i]==val) && (nums[i-1]>nums[i+1]))
+result[i]=nums[i-1];
+}
+return nums;
+}
+
+
+
+//Another Method
+
+public int[] notAlone(int[] nums, int val)
+{int l= nums.length;
+for(int i=0;i<l;i++)
+{
+if(i>0&&i<l-1)
+{if(nums[i]==val&&nums[i-1]!=val&&nums[i+1]!=val)
+{if(nums[i-1]>nums[i+1])
+nums[i]=nums[i-1];
+else
+nums[i]=nums[i+1];}}
+}
+return nums;
+}
