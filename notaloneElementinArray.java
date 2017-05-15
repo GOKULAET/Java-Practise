219bf4a0855aea21762430b4165f8c103de09da4
Gokul Raj <gokulaet@gmail.com>
2017-05-15 09:20:51 +0530

    Update notaloneElementinArray.java

--- notaloneElementinArray.java
+++ notaloneElementinArray.java
@@ -1,14 +1,12 @@
 public int[] notAlone(int[] nums, int val){
-int[] result=nums;
 for(int i=1;i<=(nums.length-2);i++){
 if((nums[i]==val) && (nums[i-1]<nums[i+1]))
-result[i]=nums[i+1];
+nums[i]=nums[i+1];
 if((nums[i]==val) && (nums[i-1]>nums[i+1]))
-result[i]=nums[i-1];
+nums[i]=nums[i-1];
 }
 return nums;
 }
-
 
 
 //Another Method
