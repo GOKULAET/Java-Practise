f4ddd971d6d20bddf329e8e3fdb7afd7bbf704d2
Gokul Raj <gokulaet@gmail.com>
2017-05-24 11:39:50 +0530

    Update maxNumberInArray.java

--- maxNumberInArray.java
+++ maxNumberInArray.java
@@ -6,3 +6,11 @@ for(int a:arr2){
 max=a>max?a:max;}
 return max;
 }
+
+
+//Another Method
+public int findMaximumAcrrossArrays(int[] arr1,int[] arr2){
+Arrays.sort(arr1);
+Arrays.sort(arr2);
+return Math.max(arr1[arr1.length-1],arr2[arr2.length-1]);
+}
