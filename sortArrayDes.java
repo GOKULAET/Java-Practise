5bbe5b42703781b773ade75fbae0e9e409e82023
Gokul Raj <gokulaet@gmail.com>
2017-05-24 10:41:11 +0530

    Create sortArrayDes.java
    Sort an array of integers in descending order using bubble sort eg: i/p : 93 46 230 27 20 15 o/p : 230 93 46 27 20 15
    Sample Input: 93 46 230 27 20 15 ->230 93 46 27 20 15 12 23 45 67->67 45 23 12
    Sample Output: 1 2 3 4->4 3 2 1

---
+++ sortArrayDes.java
@@ -0,0 +1,24 @@
+public int[] bubbleSortDescend(int[] arr) {
+int temp=0;
+for(int i=0;i<arr.length;i++){
+for(int j=i;j<arr.length;j++){
+if(arr[i]<arr[j]){
+temp=arr[i];
+arr[i]=arr[j];
+arr[j]=temp;}
+}}
+return arr;
+}
+
+//Another Method
+
+public int[] bubbleSortDescend(int[] arr) {
+for(int i=0;i<arr.length-1;i++)
+{if(arr[i]<arr[i+1])
+{arr[i]=arr[i]^arr[i+1];
+arr[i+1]=arr[i]^arr[i+1];
+arr[i]=arr[i]^arr[i+1];
+i=-1;}
+}
+return arr;
+}
