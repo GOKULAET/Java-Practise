c55de49d8a942bb022932f3bd67b58e6ce414174
Gokul Raj <gokulaet@gmail.com>
2017-06-01 20:44:16 +0530

    Update makeChocolate.java

--- makeChocolate.java
+++ makeChocolate.java
@@ -9,3 +9,32 @@ return n>small?-1:small-n;
 
 //Another Method
 
+public int makeChocolate(int small, int big, int goal) 
+{
+  int smallsize=1;
+  int bigsize=5;
+    if((smallsize*small)+(bigsize*big)<goal)
+     return -1;
+      int bigcount=goal/bigsize;
+      if(bigcount>big)
+       bigcount=big;
+     int smallcount=goal-(bigcount*bigsize);
+  return smallcount;
+}
+
+
+//Another Method
+
+public int makeChocolate(int small, int big, int goal) 
+{
+int s=small+5*big-goal;
+if(s==0)
+return small;
+if(s>0&&s>small)
+return 0;
+if(s>0)
+return s;
+else
+return -1;
+}
+
