163396fd47e72e09709246c7016c355f28f3d098
Gokul Raj <gokulaet@gmail.com>
2017-06-08 00:30:01 +0530

    Update String_printX.java
    Zoho Interview
    Print the word with odd letters as
    
    P         M
     R      A
       O  R
         G
      O    R
     R       A
    P          M

--- String_printX.java
+++ String_printX.java
@@ -1,15 +1,25 @@
-public static void printX(String s){
-        for(int i=0;i<s.length();i++){
-            for(int j=0;j<s.length();j++){
-                if(j==i)
-                    System.out.print(s.charAt(j)+"");
-                else
-                    //System.out.print(s.charAt(j));
-                    System.out.print(" ");
-                if(s.substring(j).length()==i)
-                    System.out.print(s.charAt(j)+"");
-            }
-            System.out.println(" ");
-        }
-    
-    }
+public class Test2 {
+
+	public static void main(String[] args) {
+		int[] nums={0,2,0,3,4,5,6,2,7};
+		String s1="PROGRAM";
+		System.out.println(s1);
+		printX(s1);
+	}
+	
+	public static void printX(String s){
+		        for(int i=0;i<s.length();i++){
+		             for(int j=0;j<s.length();j++){
+		            	 if(s.substring(j).length()==(i+1) )
+		                     System.out.print(s.charAt(j)+"");
+		                 if(j==i && j!=(s.length()/2))
+		                     System.out.print(s.charAt(j)+"");
+		                 else
+		                    System.out.print(" ");
+		                 
+		             }
+		             System.out.println(" ");
+		         }
+		     
+		     }
+	
