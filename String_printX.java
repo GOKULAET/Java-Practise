0aad96667c89ee652e3aedba23122addc3026d16
Gokul Raj <gokulaet@gmail.com>
2017-06-08 09:43:22 +0530

    Create String_printX.java

--- String_printX.java
+++ String_printX.java
@@ -1,7 +1,6 @@
 public class Test2 {
 
 	public static void main(String[] args) {
-		int[] nums={0,2,0,3,4,5,6,2,7};
 		String s1="PROGRAM";
 		System.out.println(s1);
 		printX(s1);
@@ -20,6 +19,31 @@ public class Test2 {
 		             }
 		             System.out.println(" ");
 		         }
-		     
-		     }
-	
+		   //Slight Modification
+public static void printX(String s){
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j<s.length();j++){
+                if(j==i)
+                    System.out.print(s.charAt(j)+"");
+                else if(s.substring(j).length()==i+1 && !(j==i))
+                    System.out.print(s.charAt(j)+"");
+                else
+                    //System.out.print(s.charAt(j));
+                    System.out.print(" ");
+                
+            }
+            System.out.println(" ");
+        }
+    
+    }
+		
+		//output:
+Program
+P     m 
+ r   a  
+  o r   
+   g    
+  o r   
+ r   a  
+P     m
+		
